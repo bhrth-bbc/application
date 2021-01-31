54b35e5b715c4ba84838abd06b4d342843ffa46c
bharath chandra <bharath_bompelly@persistent.co.in>
2021-01-31 16:07:56 +0530

    this the java application

---
+++ qwert/README.md
@@ -0,0 +1,1 @@
+## qwert Java Project
---
+++ qwert/src/main/java/bbc/maven/qwert/qwert/Application.java
@@ -0,0 +1,9 @@
+package bbc.maven.qwert.qwert;
+
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("Hello qwert");
+    }
+
+}
